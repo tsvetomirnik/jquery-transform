427dcebd2d8b9fc14b8e4cd7c59fa2a0b36f97cc
Tsvetomir Nikolov <cvetomir.nikolov@gmail.com>
2016-09-21 19:07:37 +0300

    General code improvements
    Implemented delete function logic

--- jquery.transform.js
+++ jquery.transform.js
@@ -1,62 +1,70 @@
 (function ($) {
-    'use strict';
+  'use strict';
 
-	function getStyles ($element) {
-		var styles = [];
-		 $element.attr('style').split(';').forEach(function (style) {
-			if (!style) {
-				return;
-			}
-            var styleParts = style.split(':'),
-                propName = styleParts[0].trim(),
-                propValue = styleParts[1].trim();
-			styles[propName] = propValue;
-        }, this);
-		return styles;
-	}
-	
-	function getTransforms ($element) {
-        var transformString = getStyles($element).transform.replace('  ', ' ').replace(', ', ','),
-			value = {};
-		
-		transformString.split(' ').forEach(function (transformValue) {
-			var argsString = transformValue.match(/\((.*?)\)/)[0],
-				args = argsString.replace('(', '').replace(')', '').trim().split(','),
-				name = transformValue.replace(args, '').replace('(', '').replace(')', '').trim();
-			value[name] = args;
-        }, this);
-		
-		return value;
-	}
-	
-	function getTransform ($element, functionName) {
-		return getTransforms($element)[functionName];
-	}
-	
-    function setTransform ($element, functionName, values) {
-        var transform = getTransforms($element),
-			transformString = '';
-		
-		// Update
-		transform[functionName] = values;
-		
-		// Build transform string
-		for (var transformFunc in transform) {
-			transformString += transformFunc + '(' + transform[transformFunc].join(',')  + ') ';
-		}
-		
-		// Replace transform in the styles
-		$element.css('transform', transformString);
+  function getStyles ($element) {
+    var styles = [];
+
+    $element.attr('style').split(';').forEach(function (style) {
+      if (style) {
+         var styleParts = style.split(':'),
+          propName = styleParts[0].trim(),
+          propValue = styleParts[1].trim();
+        styles[propName] = propValue;
+      }
+    }, this);
+
+    return styles;
+  }
+
+  function getTransform ($element) {
+     var transformString = getStyles($element).transform.replace(' ', '').replace(')', ') ').trim(),
+      transformFunctions = transformString.split(' '),
+      value = {};
+
+    transformFunctions.forEach(function (functionString) {
+      var functionParts = functionString.match(/^\s*(\w+)\s*\((.*)\)/),
+        name = functionParts[1],
+        args = functionParts[2].split(',');
+      value[name] = args;
+    }, this);
+
+    return value;
+  }
+
+  function getTransformFunction ($element, functionName) {
+    return getTransform($element)[functionName];
+  }
+
+  function setTransformFunction ($element, functionName, values) {
+    var transform = getTransform($element),
+      transformString = '',
+      transformFunc;
+
+    if (values) {
+      // Add or Update
+      transform[functionName] = values;
+    } else {
+      // Delete
+      delete transform[functionName];
     }
-	
-	// Export
-	$.fn.transform = function (functionName, values) {
-		var $element = $(this);
-		if (values && values.length) {
-			return setTransform($element, functionName, values);
-		} else {
-			return getTransform($element, functionName);
-		}
-	};
 
-} (jQuery));
+    // Build transform string
+    for (transformFunc in transform) {
+      transformString += transformFunc + '(' + transform[transformFunc].join(',')  + ') ';
+    }
+
+    // Replace transform in the styles
+    $element.css('transform', transformString);
+  }
+
+  // Export
+  $.fn.transform = function (functionName, values){
+    var $element = $(this);
+    if (arguments.length === 1) {
+      return getTransformFunction($element, functionName);
+    } else {
+      return setTransformFunction($element, functionName, values);
+    }
+  };
+
+}(jQuery));
